335def7e48767a165e334f721a4725c5349055c5
yasiralakus <yasiralakus11@gmail.com>
2025-03-10 20:37:31 +0300

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+
+    return (
+        <div className="full-page">
+
+            <div className="main-box">
+
+                <img src="./images/illustration-article.svg" alt="" />
+
+                <div className="content">
+
+                    <h3>Learning</h3>
+
+                    <h2>Published 21 Dec 2023</h2>
+
+                    <h1>HTML & CSS foundations</h1>
+
+                    <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+
+                </div>
+
+                <div className="user">
+                    <img src="./images/image-avatar.webp" alt="" />
+                    <p>Greg Hooper</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
